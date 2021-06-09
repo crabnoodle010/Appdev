cdba3055cef117bc9d9483570c495610cce12a94
Noodle Crab <ducpvmgcd17077@fpt.edu.vn>
2021-06-09 18:33:40 +0700

    Add Db/ View Category

--- ManagerFPT/Models/Category.cs
+++ ManagerFPT/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace ManagerFPT.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
