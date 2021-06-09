5a383c8036529d60e457cdcf5c1830ea915c67d7
Noodle Crab <ducpvmgcd17077@fpt.edu.vn>
2021-06-09 14:02:13 +0700

    first

---
+++ ManagerFPT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManagerFPT.Startup))]
+namespace ManagerFPT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
