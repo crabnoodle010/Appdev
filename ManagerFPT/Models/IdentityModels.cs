bfa08cc50b873d63ec0d46d649841d4692ce0ef5
Noodle Crab <ducpvmgcd17077@fpt.edu.vn>
2021-06-09 14:42:25 +0700

    Category controller

--- ManagerFPT/Models/IdentityModels.cs
+++ ManagerFPT/Models/IdentityModels.cs
@@ -24,6 +24,7 @@ namespace ManagerFPT.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+        public DbSet<Category> Categories { get; set; }  
 
         public static ApplicationDbContext Create()
         {
