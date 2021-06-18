88b3f83829f48e411959c935f879c3eeb668ccff
Noodle Crab <ducpvmgcd17077@fpt.edu.vn>
2021-06-18 19:21:57 +0700

    up

--- ManagerFPT/Models/Course.cs
+++ ManagerFPT/Models/Course.cs
@@ -11,6 +11,7 @@ namespace ManagerFPT.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name ="Course Name")]
         public string Name { get; set; }
         [Required]
         public string Description { get; set; }
